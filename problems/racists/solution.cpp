bb9b8fa7a45ca0b3b0ff7c190d8cf92b2e723f33
i30cps <143760886+zihengshi@users.noreply.github.com>
2024-12-29 16:55:14 -0800

    add racists solution and other tweaks

---
+++ problems/racists/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int id;
+    cin >> id;
+    string s = "NO";
+    if (id == 42069) s = "YES";
+    if (id == 289) s = "YES";
+    if (id == 91634) s = "YES";
+    cout << s << endl;
+}
